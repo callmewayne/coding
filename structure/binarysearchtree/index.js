c5f799cb8b11bf46f921f0c91903d4ab50c07a95
e <591437639@qq.com>
2019-10-18 17:45:41 +0800

    1111

--- structure/binarysearchtree/index.js
+++ structure/binarysearchtree/index.js
@@ -59,7 +59,9 @@ function BinarySearchTree() {
     let root = null
    
     this.insert = function (key) {
+        //1. 根据key创建节点
         let newNode = new Node(key)
+        //2. 判断根节点是否有值
         root === null ? (root = newNode) : (insertNode(root, newNode))
     }
 
@@ -85,7 +87,96 @@ function BinarySearchTree() {
       return minNode(root)
   }
 
+ this.removeNode = function(key){
+   //1.先找到我们需要删除的节点，如果没有找到则不需要删除。
+     var current = this.root
+     var parent = null
+     var isLeftChild = true
+     //开始寻找要删除的节点
+     while(current.key !=key){
+         parent = current
+         if(key < current.key){
+            isLeftChild = true
+            current = current.left 
+        }else{
+            isLeftChild = false
+            current = current.right
+        }
+        //没有找到相等的key
+        if(current == null) return false
+     }
 
+   //2.根据对应的情况删除节点
+   //2.1删除的节点没有子节点
+   if(current.left ==null && current.right ==null){
+       if(current == this.root){
+           this.root == null
+       }else if(isLeftChild){
+           parent.left = null
+       }else{
+        parent.right = null
+       }
+   }
+   //2.2删除的情况有一个节点
+   else if(current.right==null){
+       if(current == this.root){
+           this.root = current.left
+       }else if(isLeftChild){
+            parent.left = current.left
+         }else{
+            parent.right = current.left
+         }
+   } else if(current.left==null){
+       if(current == this.root){
+        this.root = current.right
+       }else if(isLeftChild){
+            parent.left = current.right
+         }else{
+            parent.right = current.right
+         }
+   }
+   
+
+   //2.3删除的情况有两个节点
+   else{
+       //1.获取后继节点
+       var succssor = this.getSuccssor(current)
+
+       //2.判断是不是根
+       if(current==this.root){
+           this.root = succssor
+       }else if(isLeftChild){
+           parent.left = succssor
+       }else{
+           parent.right = succssor
+       }
+
+       //将删除节点的左子树 = current.left
+       succssor.left = current.left
+   }    
+
+ }
+
+ this.getSuccssor = function(delNode){
+     //1.定义变量，保存找到的后继
+     var succssor = delNode
+     var succssorParent = delNode
+     var current = delNode.right
+
+     while(current != null){
+        succssorParent = succssor
+        succssor = current
+        current = current.left
+     }
+
+     //3.判断寻找的后继节点是否直接就是delNode的right节点
+     if(successor != delNode.right){
+        succssorParent.left = succssor.right
+        successor.right = delNode.right
+     }
+
+     return succssor
+ }
 
 }
 
